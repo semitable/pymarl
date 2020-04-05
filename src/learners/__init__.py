57c8589cc5f63ff22602d030fc85116c199ffeb8
Filippos Christianos <f.christianos@ed.ac.uk>
2020-04-05 13:05:57 +0100

    Adds centralv

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,9 +1,11 @@
 from .q_learner import QLearner
 from .coma_learner import COMALearner
 from .qtran_learner import QLearner as QTranLearner
+from .actor_critic_learner import ActorCriticLearner
 
 REGISTRY = {}
 
 REGISTRY["q_learner"] = QLearner
 REGISTRY["coma_learner"] = COMALearner
 REGISTRY["qtran_learner"] = QTranLearner
+REGISTRY["actor_critic_learner"] = ActorCriticLearner
